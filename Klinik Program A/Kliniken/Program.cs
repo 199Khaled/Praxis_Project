2abf1b6c227145eb06283533297b920c18726431
Khaled Alawedat <119186887+199Khaled@users.noreply.github.com>
2024-10-11 16:59:24 +0200

    Update Program.cs

--- Klinik Program A/Kliniken/Program.cs
+++ Klinik Program A/Kliniken/Program.cs
@@ -14,6 +14,7 @@ namespace Kliniken
         [STAThread]
         static void Main()
         {
+            //Hallo Khaled
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             //Application.Run(new frmAktualisierenUndNeueArztHinzufügen());
